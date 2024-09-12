9ded6847c08c8ab4ddc31b826d686d62288c9a4b
g0729 <151703074+g0729@users.noreply.github.com>
2024-09-12 17:07:49 +0900

    add_start_buttons

--- mysite/dialog/consumer.py
+++ mysite/dialog/consumer.py
@@ -21,18 +21,18 @@ class DialogConsumer(AsyncJsonWebsocketConsumer):
         pred, pred_sentence = predict(message)
         print(pred_sentence, mode)
 
-        if mode == 0 : # 답변 주고 끝
+        if mode == 0:  # 답변 주고 끝
             return_message, button_lst, mode = FindAnswer(pred)
-        elif mode == 1 : 
+        elif mode == 1:
             return_message, button_lst, mode = FindAnswer(pred)
-        elif mode == 2 : # 열람실 현황 크롤링 후 return
-            return_message = "열람실 현황 크롤링" #Scrapper.get_studyroom_status()
+        elif mode == 2:  # 열람실 현황 크롤링 후 return
+            return_message = "열람실 현황 크롤링"  # Scrapper.get_studyroom_status()
             button_lst = []
             mode = 0
-        elif mode == 3 : # 연락처 크롤링 후 return
-            return_message = "연락처 크롤링" #Scrapper.get_phone_number()
+        elif mode == 3:  # 연락처 크롤링 후 return
+            return_message = "연락처 크롤링"  # Scrapper.get_phone_number()
             button_lst = []
             mode = 0
-        
+
         print(return_message, mode, button_lst)
-        await self.send(text_data=json.dumps({"message": return_message, "mode" : mode, "button" : button_lst}))
+        await self.send(text_data=json.dumps({"message": return_message, "mode": mode, "button": button_lst}))
--- mysite/static/bot.js
+++ mysite/static/bot.js
@@ -2,29 +2,38 @@ const $chatBox = document.getElementById("chat-box");
 const $question = document.querySelector("#question");
 const $submitBtn = document.querySelector("#ask-button");
 const webSocket = new WebSocket("ws://" + window.location.host + "/ws/dialog");
+const $$buttons = document.querySelectorAll(".button");
 let md = 0;
+let received = true;
+//button function
+Array.from($$buttons).forEach((button) => {
+	button.addEventListener("click", (event) => {
+		submitMessage(event.target.textContent);
+	});
+});
 
 //WebSocket methods START
 
 //receive data from server
 webSocket.onmessage = function (event) {
+	received = true;
 	const data = JSON.parse(event.data);
-	if(data.mode == 0){
+	if (data.mode == 0) {
 		md = 0;
 		addMessage("Bot", data.message);
-	}
-	else if(data.mode == 1){  // 학사일정
+	} else if (data.mode == 1) {
+		// 학사일정
 		md = 0;
 		addButton(data.button, data.mode);
-	}
-	else if(data.mode == 2){ // 열람실 현황
+	} else if (data.mode == 2) {
+		// 열람실 현황
 		md = 2;
 		addButton(data.button, data.mode);
-	}else if (data.mode == 3) { // 연락처
+	} else if (data.mode == 3) {
+		// 연락처
 		md = 3;
 		addContactButton();
-	} 
-		
+	}
 };
 
 //WebSocket closed unexpectedly
@@ -40,25 +49,20 @@ $question.onkeyup = function (event) {
 	}
 };
 
-$submitBtn.onclick = function (event) {
+$submitBtn.onclick = function () {
 	const message = $question.value;
 	if (message === "") return;
-	addMessage("You", message);
-	webSocket.send(JSON.stringify({ "message": message , "mode" : md}));
-	$question.value = "";
+	submitMessage(message);
 };
 
+function submitMessage(message) {
+	if (!received) return;
+	received = false;
+	addMessage("You", message);
+	webSocket.send(JSON.stringify({ message: message, mode: md }));
+	$question.value = "";
+}
 //WebSocket methods END
-
-
-document.addEventListener("DOMContentLoaded", function () {
-	// 페이지가 로드될 때 환영 메시지를 추가
-	$question.focus();	
-	addMessage(
-		"Bot",
-		"안녕하세요! 홍익대학교 챗봇 Hongik-gpt입니다:)<br>학교에 대해 궁금한 것을 물어보세요!"
-	);
-});
 
 function addMessage(sender, message) {
 	const messageDiv = document.createElement("div");
@@ -72,8 +76,8 @@ function addMessage(sender, message) {
 
 function addButton(button_list, mode) {
 	const buttonDiv = document.createElement("div");
-	
-	button_list.forEach(element => {
+
+	button_list.forEach((element) => {
 		const button = document.createElement("button");
 
 		button.textContent = element;
@@ -91,30 +95,29 @@ function addButton(button_list, mode) {
 function handleButtonClick(option, mode) {
 	// 사용자가 클릭한 버튼 옵션을 대화창에 표시
 	addMessage("You", option); // 사용자의 옵션을 질문처럼 처리
-	webSocket.send(JSON.stringify({ "message": option, "mode": mode }));
+	webSocket.send(JSON.stringify({ message: option, mode: mode }));
 }
 
-function addContactButton(){
+function addContactButton() {
 	const buttonDiv = document.createElement("div");
 
 	const button1 = document.createElement("button");
 	button1.textContent = "교직원/교수 연락처";
-	button1.onclick = function(){
-		addMessage("Bot", "검색하고 싶은 교직원/교수를 입력해주세요")
-	}
+	button1.onclick = function () {
+		addMessage("Bot", "검색하고 싶은 교직원/교수를 입력해주세요");
+	};
 
 	const button2 = document.createElement("button");
 	button2.textContent = "학과 연락처";
 	button2.onclick = function () {
-		addMessage("Bot","검색하고 싶은 학과를 입력해주세요")
-	}
+		addMessage("Bot", "검색하고 싶은 학과를 입력해주세요");
+	};
 
 	buttonDiv.appendChild(button1);
 	buttonDiv.appendChild(button2);
 	$chatBox.appendChild(buttonDiv);
 	$chatBox.scrollTop = $chatBox.scrollHeight;
 }
-
 
 // function askQuestion() {
 // 	// 사용자 입력 값 가져오기
