135d3560e8643c1bae33c1bb9bf7a188bdd91172
g0729 <151703074+g0729@users.noreply.github.com>
2024-10-12 15:49:16 +0900

    이메일 인증 추가

--- mysite/common/forms.py
+++ mysite/common/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserForm(UserCreationForm):
@@ -9,3 +10,13 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "password1", "password2", "email"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        email = cleaned_data.get("email")
+
+        if email and not email.endswith("@naver.com"):
+            self.add_error("email", ValidationError("이메일은 @naver.com 도메인만 사용할 수 있습니다."))
+
+        return cleaned_data
